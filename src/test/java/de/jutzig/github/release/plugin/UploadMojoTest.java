e42192220781233cbb538409bfa4603e348f0eab
Łukasz Wiecheć <lukasz@wiechec.eu>
2016-04-01 11:09:47 +0200

    attempting to create a test

--- src/test/java/de/jutzig/github/release/plugin/UploadMojoTest.java
+++ src/test/java/de/jutzig/github/release/plugin/UploadMojoTest.java
@@ -2,17 +2,39 @@ package de.jutzig.github.release.plugin;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
+import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.io.File;
 
-import static org.junit.Assert.assertEquals;
+import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.WithoutMojo;
+import org.apache.maven.plugin.testing.resources.TestResources;
 
 public class UploadMojoTest {
 
+    @Rule
+    public MojoRule rule = new MojoRule()
+    {
+      @Override
+      protected void before() throws Throwable
+      {
+      }
+
+      @Override
+      protected void after()
+      {
+      }
+    };
+
+    @Rule
+    public TestResources resources = new TestResources();
+
 	private Map<String, String> computeRepositoryIdData;
 
-	@Before
+    @Before
 	public void setUp() throws Exception {
 		computeRepositoryIdData = new HashMap<String, String>();
 
@@ -31,13 +53,25 @@ public class UploadMojoTest {
 		computeRepositoryIdData.put("scm:git:https://github.com/jutzig/github-release-plugin", "jutzig/github-release-plugin");
 		computeRepositoryIdData.put("scm:git|https://github.com/jutzig/github-release-plugin", "jutzig/github-release-plugin");
 		computeRepositoryIdData.put("https://github.com/jutzig/github-release-plugin", "jutzig/github-release-plugin");
+        //        computeRepositoryIdData.put("http://some-github-enterprise-server.domain/user/repository.git", "user/repository");
 	}
 
 	@Test
 	public void testComputeRepositoryId() throws Exception {
+
+        File projectCopy = this.resources.getBasedir( "github.com-project" );
+        File pom = new File(projectCopy, "pom.xml");
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+        UploadMojo mojo = (UploadMojo) rule.lookupMojo( "", pom);
+        assertNotNull(mojo);
+
 		for (String source : computeRepositoryIdData.keySet()) {
 			String expected = computeRepositoryIdData.get(source);
-			assertEquals(source, expected, UploadMojo.computeRepositoryId(source));
+			assertEquals(source, expected, mojo.computeRepositoryId(source));
 		}
+
+        mojo.execute();
 	}
 }
