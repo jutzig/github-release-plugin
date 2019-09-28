ee6acc592bc6b6aa648e7ac6b9d77dab0ffd3d8d
Torsten Juergeleit <torsten.juergeleit@gmail.com>
2019-09-28 18:55:34 +0200

    updates plugins and dependencies to their latest versions
    commons-lang is not needed because commons-lang3 is a transitive dependency via github-api

--- src/main/java/de/jutzig/github/release/plugin/UploadMojo.java
+++ src/main/java/de/jutzig/github/release/plugin/UploadMojo.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.FileSet;
 import org.apache.maven.plugin.AbstractMojo;
