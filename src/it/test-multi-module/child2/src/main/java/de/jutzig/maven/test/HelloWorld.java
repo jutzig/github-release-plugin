b1a086f4016d3c53737122237c5def5c8982d518
Torsten Juergeleit <torsten.juergeleit@gmail.com>
2019-09-29 01:41:01 +0200

    adds additional multi-module integration test
    This test requires fix for #40 due to "deleteRelease=false".

---
+++ src/it/test-multi-module/child2/src/main/java/de/jutzig/maven/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package de.jutzig.maven.test;
+
+public class HelloWorld
+{
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+    
+}
---
+++ src/it/test-multi-module/verify.groovy
@@ -0,0 +1,2 @@
+// TODO: Add a proper check, but, basically, if the invoker build fails, that would be enough of a failure.
+return true
